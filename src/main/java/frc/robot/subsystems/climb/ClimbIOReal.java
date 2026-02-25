89fd6c3665291d7e2a860071b97cd2a99125fac9
drakeerv <drakeerv@outlook.com>
2026-02-25 10:26:16 -0800

    Update climb code

--- src/main/java/frc/robot/subsystems/climb/ClimbIOReal.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOReal.java
@@ -7,6 +7,8 @@
 
 package frc.robot.subsystems.climb;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Volts;
 import static frc.robot.subsystems.climb.ClimbConstants.*;
 import static frc.robot.util.SparkUtil.*;
 
@@ -18,24 +20,19 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.units.measure.Voltage;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 // TODO: Need to verify how spark max limits work
 
 public class ClimbIOReal implements ClimbIO {
-  private final SparkMax liftSpark = new SparkMax(liftMotorCanId, MotorType.kBrushed);
-  private final SparkMax rotateSpark = new SparkMax(rotateMotorCanId, MotorType.kBrushed);
+  private final SparkMax spark = new SparkMax(liftMotorCanId, MotorType.kBrushed);
 
-  private final SparkLimitSwitch liftUpperLimit = liftSpark.getForwardLimitSwitch();
-  private final SparkLimitSwitch liftLowerLimit = liftSpark.getReverseLimitSwitch();
-
-  private final SparkLimitSwitch rotateMaxLimit = liftSpark.getForwardLimitSwitch();
-  private final SparkLimitSwitch rotateMinLimit = liftSpark.getReverseLimitSwitch();
+  private final SparkLimitSwitch upperLimit = spark.getForwardLimitSwitch();
+  private final SparkLimitSwitch lowerLimit = spark.getReverseLimitSwitch();
 
   private final Debouncer liftConnectedDebounce =
-      new Debouncer(0.5, Debouncer.DebounceType.kFalling);
-  private final Debouncer rotateConnectedDebounce =
       new Debouncer(0.5, Debouncer.DebounceType.kFalling);
 
   private boolean sparkStickyFault = false;
@@ -44,82 +41,43 @@ public class ClimbIOReal implements ClimbIO {
     var liftCfg = new SparkMaxConfig();
     liftCfg
         .idleMode(IdleMode.kBrake)
-        .smartCurrentLimit(liftMotorCurrentLimit)
+        .smartCurrentLimit((int) liftMotorCurrentLimit.in(Amps))
         .voltageCompensation(12.0);
     liftCfg.signals.appliedOutputPeriodMs(20).busVoltagePeriodMs(20).outputCurrentPeriodMs(20);
     tryUntilOk(
-        liftSpark,
+        spark,
         5,
         () ->
-            liftSpark.configure(
+            spark.configure(
                 liftCfg, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
-
-    var rotateCfg = new SparkMaxConfig();
-    rotateCfg
-        .idleMode(IdleMode.kBrake)
-        .smartCurrentLimit(rotateMotorCurrentLimit)
-        .voltageCompensation(12.0);
-    rotateCfg.signals.appliedOutputPeriodMs(20).busVoltagePeriodMs(20).outputCurrentPeriodMs(20);
-    tryUntilOk(
-        rotateSpark,
-        5,
-        () ->
-            rotateSpark.configure(
-                rotateCfg, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
   }
 
   @Override
   public void updateInputs(ClimbIOInputs inputs) {
     sparkStickyFault = false;
     ifOk(
-        liftSpark,
-        new DoubleSupplier[] {liftSpark::getAppliedOutput, liftSpark::getBusVoltage},
-        (vals) -> inputs.liftAppliedVolts = vals[0] * vals[1]);
-    ifOk(liftSpark, liftSpark::getOutputCurrent, (v) -> inputs.liftCurrentAmps = v);
+        spark,
+        new DoubleSupplier[] {spark::getAppliedOutput, spark::getBusVoltage},
+        (vals) -> inputs.appliedVoltage = Volts.of(vals[0] * vals[1]));
+    ifOk(spark, spark::getOutputCurrent, (v) -> inputs.appliedCurrent = Amps.of(v));
     ifOk(
-        liftSpark,
-        new BooleanSupplier[] {liftUpperLimit::isPressed, liftLowerLimit::isPressed},
+        spark,
+        new BooleanSupplier[] {upperLimit::isPressed, lowerLimit::isPressed},
         (vals) -> {
-          inputs.liftUpperLimit = vals[0];
-          inputs.liftLowerLimit = vals[1];
+          inputs.upperLimit = vals[0];
+          inputs.lowerLimit = vals[1];
         });
-    inputs.liftConnected = liftConnectedDebounce.calculate(!sparkStickyFault);
-
-    sparkStickyFault = false;
-    ifOk(
-        rotateSpark,
-        new DoubleSupplier[] {rotateSpark::getAppliedOutput, rotateSpark::getBusVoltage},
-        (vals) -> inputs.rotateAppliedVolts = vals[0] * vals[1]);
-    ifOk(rotateSpark, rotateSpark::getOutputCurrent, (v) -> inputs.rotateCurrentAmps = v);
-    ifOk(
-        rotateSpark,
-        new BooleanSupplier[] {rotateMaxLimit::isPressed, rotateMinLimit::isPressed},
-        (vals) -> {
-          inputs.rotateMinLimit = vals[0];
-          inputs.rotateMaxLimit = vals[1];
-        });
-    inputs.rotateConnected = rotateConnectedDebounce.calculate(!sparkStickyFault);
+    inputs.connected = liftConnectedDebounce.calculate(!sparkStickyFault);
   }
 
   @Override
-  public void setLiftOpenLoop(double volts) {
-    boolean atUpper = !liftUpperLimit.isPressed();
-    boolean atLower = !liftLowerLimit.isPressed();
-    if ((volts > 0.0 && atUpper) || (volts < 0.0 && atLower)) {
-      liftSpark.setVoltage(0.0);
+  public void setLiftVoltage(Voltage voltage) {
+    boolean atUpper = !upperLimit.isPressed();
+    boolean atLower = !lowerLimit.isPressed();
+    if ((voltage.gt(Volts.of(0.0)) && atUpper) || (voltage.lt(Volts.of(0.0)) && atLower)) {
+      spark.setVoltage(0.0);
     } else {
-      liftSpark.setVoltage(volts);
-    }
-  }
-
-  @Override
-  public void setRotateOpenLoop(double volts) {
-    boolean atMax = !rotateMaxLimit.isPressed();
-    boolean atMin = !rotateMinLimit.isPressed();
-    if ((volts > 0.0 && atMax) || (volts < 0.0 && atMin)) {
-      rotateSpark.setVoltage(0.0);
-    } else {
-      rotateSpark.setVoltage(volts);
+      spark.setVoltage(voltage);
     }
   }
 }
