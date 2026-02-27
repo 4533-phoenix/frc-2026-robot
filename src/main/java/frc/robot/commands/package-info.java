563da9e58b384b0eed84613784fac0d21dd6d6fc
drakeerv <drakeerv@outlook.com>
2026-02-27 12:53:00 -0800

    Add package-info

---
+++ src/main/java/frc/robot/commands/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains factory classes for creating commands used to interact with subsystems.
+ */
+package frc.robot.commands;
---
+++ src/main/java/frc/robot/subsystems/intake/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains the Intake subsystem, responsible for collecting game pieces
+ * from the field.
+ */
+package frc.robot.subsystems.intake;
