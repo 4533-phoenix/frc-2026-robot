4bb2446ba558151f51d2e732cc96783774f8d23f
drakeerv <drakeerv@outlook.com>
2026-04-13 21:19:06 -0400

    Fix license header typo

--- scripts/offset_finder.py
+++ scripts/offset_finder.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2026 FRC Team 4533 (Phoenix)
 #
-# Use of this source code is governed by a BSD-style
+# Use of this source code is governed by a BSD
 # license that can be found in the LICENSE file
 # at the root directory of this project.
 
